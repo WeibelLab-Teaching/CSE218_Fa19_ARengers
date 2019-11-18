ab9112a627d3a1f8495a3aab1b8ae4b375de8b9a
ulrichzglr <56712260+ulrichzglr@users.noreply.github.com>
2019-11-18 00:24:02 -0800

    Added Buttons to switch to Zoom and Subtitle Scene

---
+++ Assets/SceneChange.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+
+    public void LoadSubtitle()
+    {
+        Debug.Log("Scene to load: Subtitle");
+        SceneManager.LoadScene("Init scene");
+    }
+
+
+    public void LoadZoom()
+    {
+        Debug.Log("Scene to load: Zoom");
+        SceneManager.LoadScene("UI scene");
+    }
+
+}

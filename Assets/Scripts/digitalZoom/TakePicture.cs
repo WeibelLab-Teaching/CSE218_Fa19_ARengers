7b52719e5dde91910c6058d5f8c38a3928727901
helloxdemi <chocoanna1128@gmail.com>
2019-11-19 12:41:17 -0800

    Put photos onto imagebox whenever we clikc on the area of interest. However, some bugs for taking several pictures need to be fixed.

--- Assets/Scripts/digitalZoom/TakePicture.cs
+++ Assets/Scripts/digitalZoom/TakePicture.cs
@@ -8,8 +8,9 @@ public class TakePicture: MonoBehaviour
     UnityEngine.XR.WSA.WebCam.PhotoCapture photoCaptureObject = null;
     public GameObject quad;
 
-    public void TakeAShot ()
+    public void takePicture ()
     {
+        // Debug.Log("hi 1");
         UnityEngine.XR.WSA.WebCam.PhotoCapture.CreateAsync(false, OnPhotoCaptureCreated);
     }
     void OnPhotoCaptureCreated(UnityEngine.XR.WSA.WebCam.PhotoCapture captureObject)
@@ -25,11 +26,13 @@ public class TakePicture: MonoBehaviour
         c.pixelFormat = UnityEngine.XR.WSA.WebCam.CapturePixelFormat.BGRA32;
 
         captureObject.StartPhotoModeAsync(c, OnPhotoModeStarted);
+        // Debug.Log("hi 2");
     }
     private void OnPhotoModeStarted(UnityEngine.XR.WSA.WebCam.PhotoCapture.PhotoCaptureResult result)
     {
         if (result.success)
         {
+            // take a picture
             photoCaptureObject.TakePhotoAsync(OnCapturedPhotoToMemory);
         }
         else
@@ -37,12 +40,15 @@ public class TakePicture: MonoBehaviour
             Debug.LogError("Unable to start photo mode!");
         }
     }
+
     void OnCapturedPhotoToMemory(PhotoCapture.PhotoCaptureResult result, PhotoCaptureFrame photoCaptureFrame)
     {
         if (result.success)
         {
             Debug.Log("Photo Captured");
             // Create our Texture2D for use and set the correct resolution
+            // GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            Renderer quadRenderer = quad.GetComponent<Renderer>() as Renderer;
             Resolution cameraResolution = PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
             Texture2D targetTexture = new Texture2D(cameraResolution.width, cameraResolution.height);
 
@@ -53,20 +59,24 @@ public class TakePicture: MonoBehaviour
 
             Texture2D newImage = CropImage(targetTexture, 0, 0, cameraResolution.width, cameraResolution.height);
 
-            Renderer quadRenderer = quad.GetComponent<Renderer>() as Renderer;
+           // Renderer quadRenderer = quad.GetComponent<Renderer>() as Renderer;
             quadRenderer.material = new Material(Shader.Find("Unlit/Texture"));
 
-            // quad.transform.parent = this.transform;
-            // quad.transform.localPosition = new Vector3(0.0f, 0.0f, 3.0f);
+            quad.transform.parent = this.transform;
+          //  quad.transform.localPosition = new Vector3(0.0f, 0.0f, 3.0f);
 
             quadRenderer.material.SetTexture("_MainTex", newImage);
-            Debug.Log("awesome");
         }
         // Clean up
+        Debug.Log("Cleaning it up");
+
         photoCaptureObject.StopPhotoModeAsync(OnStoppedPhotoMode);
     }
     private Texture2D CropImage(Texture2D old, int x, int y, int width, int height)
     {
+        // it works, we just need to find the real resolution for the Hololens' camera
+        // and crop the correct area we want. 
+             
         Color[] pix = old.GetPixels(0, 0, width, height);
         Texture2D newTexture = new Texture2D(width, height);
         newTexture.SetPixels(pix);
@@ -75,6 +85,7 @@ public class TakePicture: MonoBehaviour
     }
     void OnStoppedPhotoMode(UnityEngine.XR.WSA.WebCam.PhotoCapture.PhotoCaptureResult result)
     {
+        Debug.Log("Camera closed");
         photoCaptureObject.Dispose();
         photoCaptureObject = null;
     }

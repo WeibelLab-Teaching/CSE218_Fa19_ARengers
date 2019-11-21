7ae36e9d6e0cdf58e6996814d80d65d2839ad8db
ruw001 <i-ru.wang@clobotics.com>
2019-11-21 00:15:10 -0800

    port the dictation system to azure service

--- Assets/Transcribe.cs
+++ Assets/Transcribe.cs
@@ -1,64 +1,92 @@
-﻿using Microsoft.MixedReality.Toolkit.Input;
-using UnityEditor;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Windows.Speech;
+using Microsoft.CognitiveServices.Speech;
 
 public class Transcribe : MonoBehaviour
 {
-    // Unity built-in
-    public TMPro.TextMeshPro hypothesis;
-    public TMPro.TextMeshPro result;
-    public Text status;
-    public int mode;
-    private DictationRecognizer m_DictationRecognizer;
+    // Hook up the two properties below with a Text and Button object in your UI.
+    public TMPro.TextMeshPro outputText;
 
-    // Start is called before the first frame update
-    void Start()
+    private object threadLocker = new object();
+    private bool waitingForReco;
+    private string message;
+
+    private bool micPermissionGranted = false;
+
+    public async void ButtonClick()
     {
-        m_DictationRecognizer = new DictationRecognizer();
+        // Creates an instance of a speech config with specified subscription key and service region.
+        // Replace with your own subscription key and service region (e.g., "westus").
+        var config = SpeechConfig.FromSubscription("9b841b86d19a46bb9b35701488e3bac3", "westus");
 
-        m_DictationRecognizer.DictationResult += (text, confidence) =>
+        // Make sure to dispose the recognizer after use!
+        using (var recognizer = new SpeechRecognizer(config))
         {
-            //Debug.LogFormat("Dictation result: {0}", text);
-            //m_Recognitions.text += text + "\n";
-            result.text = text;
-            status.text = "Listening...";
-        };
+            lock (threadLocker)
+            {
+                waitingForReco = true;
+            }
 
-        m_DictationRecognizer.DictationHypothesis += (text) =>
-        {
-            //Debug.LogFormat("Dictation hypothesis: {0}", text);
-            //m_Hypotheses.text += text;
-            hypothesis.text = text;
-            status.text = "Thinking...";
-        };
+            // Starts speech recognition, and returns after a single utterance is recognized. The end of a
+            // single utterance is determined by listening for silence at the end or until a maximum of 15
+            // seconds of audio is processed.  The task returns the recognition text as result.
+            // Note: Since RecognizeOnceAsync() returns only a single utterance, it is suitable only for single
+            // shot recognition like command or query.
+            // For long-running multi-utterance recognition, use StartContinuousRecognitionAsync() instead.
+            var result = await recognizer.RecognizeOnceAsync().ConfigureAwait(false);
 
-        m_DictationRecognizer.DictationComplete += (completionCause) =>
-        {
-            status.text = "Sleeping...";
-            if (completionCause != DictationCompletionCause.Complete)
-                Debug.LogErrorFormat("Dictation completed unsuccessfully: {0}.", completionCause);
-        };
+            // Checks result.
+            string newMessage = string.Empty;
+            if (result.Reason == ResultReason.RecognizedSpeech)
+            {
+                newMessage = result.Text;
+            }
+            else if (result.Reason == ResultReason.NoMatch)
+            {
+                newMessage = "NOMATCH: Speech could not be recognized.";
+            }
+            else if (result.Reason == ResultReason.Canceled)
+            {
+                var cancellation = CancellationDetails.FromResult(result);
+                newMessage = $"CANCELED: Reason={cancellation.Reason} ErrorDetails={cancellation.ErrorDetails}";
+            }
 
-        m_DictationRecognizer.DictationError += (error, hresult) =>
-        {
-            Debug.LogErrorFormat("Dictation error: {0}; HResult = {1}.", error, hresult);
-        };
-
-        if (PhraseRecognitionSystem.Status == SpeechSystemStatus.Running)
-        {
-            PhraseRecognitionSystem.Shutdown();
+            lock (threadLocker)
+            {
+                message = newMessage;
+                waitingForReco = false;
+            }
         }
-
-        m_DictationRecognizer.AutoSilenceTimeoutSeconds = 100000;
-
-        m_DictationRecognizer.Start();
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        if (outputText == null)
+        {
+            Debug.LogError("outputText property is null! Assign a UI Text element to it.");
+        }
+        else
+        {
+            // Continue with normal initialization, Text and Button objects are present.
+            micPermissionGranted = true;
+            message = "";
+        }
+    }
+
     void Update()
     {
-        
+
+        lock (threadLocker)
+        {
+            if ((!waitingForReco) && micPermissionGranted)
+            {
+                ButtonClick();
+            }
+            if (outputText != null)
+            {
+                outputText.text = message;
+            }
+        }
     }
 }
